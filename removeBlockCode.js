a7800d3686215effed8af11c66ed6ecd6fa5a14c
vinhphan812 <vinhphan812@gmail.com>
2021-07-15 15:54:05 +0700

    fix algorithm remove

--- removeBlockCode.js
+++ removeBlockCode.js
@@ -1,9 +1,10 @@
 if (document.getElementsByClassName("code-expandContent").length) {
-	const codeContent = document.getElementsByClassName("code-expandContent"),
+	const codeContent = document.getElementsByClassName("code-expandContent");
+	var blockCode = document.getElementsByClassName("bbCodeBlock-expandLink"),
+		i = 0;
+	while (blockCode.length > 0) {
+		blockCode[0].remove();
+		codeContent[i] ? codeContent[i++].removeAttribute("style") : i++;
 		blockCode = document.getElementsByClassName("bbCodeBlock-expandLink");
-
-	for (var i = 0; i < codeContent.length; i++) {
-		codeContent[i].removeAttribute("style");
-		blockCode[i - i].remove();
 	}
 }
